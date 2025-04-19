7b0e2eaeb5e12988d06b5067f6a777351dded6f6
Mahdioshani <mahdioshani2003@gmail.com>
2025-04-19 12:56:58 +0330

    feat: Structure of Event Cache manager

---
+++ Storage/EventCacheManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using MessagePack;
+using odysseyAnalytics.Types;
+
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE
+using UnityEngine;
+#endif
+
+namespace odysseyAnalytics.Storage
+{
+    public class EventCacheManager
+    {
+        
+    }
+}
