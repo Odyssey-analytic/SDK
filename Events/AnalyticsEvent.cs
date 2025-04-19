0fedff6e1d534bf4f69b707189c8f271b84bdf4c
Mahdioshani <mahdioshani2003@gmail.com>
2025-04-19 13:38:19 +0330

    feat: handling event types to save in db

---
+++ Events/AnalyticsEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using SQLite;
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE
+using UnityEngine;
+#endif
+
+namespace odysseyAnalytics.Events
+{
+    public class AnalyticsEvent
+    {
+        [PrimaryKey, AutoIncrement] public int Id { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventTime { get; set; }
+        public string SessionId { get; set; }
+        public string ClientId { get; set; }
+        private string DataJson { get; set; }
+
+        [Ignore] // Prevents this from being mapped to SQLite
+        public Dictionary<string, object> Data
+        {
+            get
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, object>>(DataJson);
+            }
+            set
+            {
+                DataJson = JsonConvert.SerializeObject(value);
+            }
+        }
+    }
+}
--- Storage/EventCacheManager.cs
+++ Storage/EventCacheManager.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
-using MessagePack;
-using odysseyAnalytics.Types;
+using SQLite;
+using odysseyAnalytics.Events;
 
 #if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE
 using UnityEngine;
@@ -14,6 +12,46 @@ namespace odysseyAnalytics.Storage
 {
     public class EventCacheManager
     {
-        
+        private readonly SQLiteConnection _db;
+
+        public EventCacheManager()
+        {
+            var path = GetDatabasePath();
+            _db = new SQLiteConnection(path);
+            _db.CreateTable<AnalyticsEvent>();
+        }
+
+        private string GetDatabasePath()
+        {
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE
+            return Path.Combine(Application.persistentDataPath, "analytics.db");
+#else
+            return Path.Combine(AppContext.BaseDirectory, "analytics.db");
+#endif
+        }
+        public void SaveEvent(string name,string clientId,string sessionId, Dictionary<string, object> eventDatas)
+        {
+            var evt = new AnalyticsEvent
+            {
+                EventName = name,
+                SessionId = sessionId,
+                EventTime = DateTime.UtcNow,
+                Data = eventDatas,
+                ClientId = clientId
+            };
+            _db.Insert(evt);
+        }
+        public List<AnalyticsEvent> LoadAllEvents()
+        {
+            return _db.Table<AnalyticsEvent>().ToList();
+        }
+        public void DeleteEvent(int id)
+        {
+            _db.Delete<AnalyticsEvent>(id);
+        }
+        public void Clear()
+        {
+            _db.DeleteAll<AnalyticsEvent>();
+        }
     }
 }
