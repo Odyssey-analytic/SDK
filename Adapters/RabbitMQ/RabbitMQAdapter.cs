24b9a0a297fb271db67b02fa650e169fc2e4e450
Ali <ali.ali.samadifard@gmail.com>
2025-04-22 13:36:40 +0330

    feat: Add RabbitMQ adapter

---
+++ Adapters/RabbitMQ/RabbitMQAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using odysseyAnalytics.Core.Application.Events;
+using odysseyAnalytics.Core.Ports;
+using RabbitMQ.Client;
+
+namespace odysseyAnalytics.Adapters.RabbitMQ
+{
+    public class RabbitMQAdapter : IMessagePublisherPort
+    {
+        private IConnection _connection;
+        private IChannel _channel;
+        
+        public Task<T> PublishMessage<T>(T analyticsEvent) where T : AnalyticsEvent
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = host,
+                UserName = username,
+                Password = password,
+                Port = port,
+                VirtualHost = vHost,
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(5)
+            };
+
+            _connection =await factory.CreateConnectionAsync();
+            _channel = await _connection.CreateChannelAsync();
+        }
+
+        private RabbitMQDTO MapToDto(AnalyticsEvent evt)
+        {
+            return new RabbitMQDTO
+            {
+                EventName = evt.EventName,
+                QueueName = evt.QueueName,
+                EventTime = evt.EventTime,
+                SessionId = evt.SessionId,
+                ClientId = evt.ClientId,
+                Priority = evt.Priority,
+                Data = evt.Data
+            };
+        }
+    }
+    
+}
---
+++ Adapters/RabbitMQ/RabbitMQDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace odysseyAnalytics.Adapters.RabbitMQ
+{
+    public class RabbitMQDTO
+    {
+        public string EventName { get; set; }
+        public string QueueName { get; set; }
+        public DateTime EventTime { get; set; }
+        public string SessionId { get; set; }
+        public string ClientId { get; set; }
+        public int Priority { get; set; }
+        public Dictionary<string, object> Data { get; set; }
+    }
+}
