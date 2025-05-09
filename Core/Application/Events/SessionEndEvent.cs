bd64ac2dfbbedb7f22f8053f0ffa50a2c95dd628
Ali <ali.ali.samadifard@gmail.com>
2025-05-09 19:02:42 +0330

    fix: Edit start and end session events to handle nullable data

--- Core/Application/Events/SessionEndEvent.cs
+++ Core/Application/Events/SessionEndEvent.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace odysseyAnalytics.Core.Application.Events
 {
     public class SessionEndEvent : AnalyticsEvent
     {
-        private string platform;
-
-        public SessionEndEvent(string platform, string eventName, string queueName, DateTime eventTime,
-            string sessionId, string clientId, int priority, Dictionary<string, string> data, int id = -1) : base(
-            eventName, queueName, eventTime, sessionId, clientId, priority, data, id)
+        public SessionEndEvent(string queueName, DateTime eventTime,
+            string sessionId, string clientId, int priority, Dictionary<string, string> data = null,
+            int id = -1) : base(
+            queueName, eventTime, sessionId, clientId, priority, data, id)
         {
-            this.platform = platform;
+            EventType = SESSION_END_EVENT_TYPE;
+            _data["time"] = EventTime.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            _data["session"] = SessionId;
+            _data["client"] = ClientId;
+            _dataJson = JsonConvert.SerializeObject(_data);
         }
     }
 }
--- Core/Application/Events/SessionStartEvent.cs
+++ Core/Application/Events/SessionStartEvent.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace odysseyAnalytics.Core.Application.Events
 {
     public class SessionStartEvent : AnalyticsEvent
     {
-        public SessionStartEvent(string eventName, string queueName, DateTime eventTime, string sessionId,
-            string clientId, int priority, Dictionary<string, string> data, int id = -1) : base(eventName, queueName,
+        private string platform;
+
+        public SessionStartEvent(string queueName, DateTime eventTime, string sessionId,
+            string clientId, int priority, string platform, Dictionary<string, string> data = null, int id = -1) : base(
+            queueName,
             eventTime, sessionId, clientId, priority, data, id)
         {
+            this.platform = platform;
+            EventType = SESSION_START_EVENT_TYPE;
+            _data["time"] = EventTime.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            _data["session"] = SessionId;
+            _data["client"] = ClientId;
+            _data["platform"] = platform;
+            _dataJson = JsonConvert.SerializeObject(_data);
         }
     }
 }
