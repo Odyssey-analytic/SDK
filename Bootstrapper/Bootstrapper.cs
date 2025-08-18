92080e7ffe7333da2bc9c49e7800ea2988712446
Ali <ali.ali.samadifard@gmail.com>
2025-08-18 23:01:45 +0330

    feat: Add boot strapper to the project

---
+++ Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using odysseyAnalytics.Core.Ports;
+using odysseyAnalytics.Core.Application.Session;
+using odysseyAnalytics.Adapters.Logger;
+using odysseyAnalytics.Adapters.RabbitMQ;
+using odysseyAnalytics.Adapters.REST;
+using odysseyAnalytics.Adapters.Sqlite;
+
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE
+                        using UnityEngine;
+#endif
+
+namespace Bootstrapper
+{
+    public class Bootstrapper
+    {
+        private IGatewayPort gatewayPort;
+        private IMessagePublisherPort messagePublisher;
+        private IConnectablePublisher connection;
+        private ILogger logger;
+        private IDatabasePort databasePort;
+        private SessionHandler session;
+
+
+        public Bootstrapper()
+        {
+            logger = new DefaultLogger();
+            messagePublisher = new RabbitMQAdapter();
+            gatewayPort = new RESTAdapter("https://odysseyanalytics.ir/api/");
+            connection = messagePublisher as IConnectablePublisher;
+
+
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE
+                        databasePort = new SqliteAdapter(Path.Combine(Application.persistentDataPath, "events.db"));
+#else
+            databasePort = new SqliteAdapter("./events.db");
+#endif
+        }
+
+        public Bootstrapper SetGatewayPort(IGatewayPort port)
+        {
+            gatewayPort = port;
+            return this;
+        }
+
+        public Bootstrapper SetMessagePublisher(IMessagePublisherPort port)
+        {
+            messagePublisher = port;
+            return this;
+        }
+
+        public Bootstrapper SetIConnectablePublisher(IConnectablePublisher connection)
+        {
+            this.connection = connection;
+            return this;
+        }
+
+        public Bootstrapper SetDatabasePort(IDatabasePort port)
+        {
+            this.databasePort = port;
+            return this;
+        }
+
+        public Bootstrapper SetLogger(ILogger logger)
+        {
+            this.logger = logger;
+            return this;
+        }
+
+        public async Task<SessionHandler> InitializeSession(string token)
+        {
+            session = new SessionHandler(gatewayPort, messagePublisher, connection, logger, databasePort, token);
+            await session.InitializeSessionAsync();
+            return session;
+        }
+    }
+}
--- Core/obj/Debug/netstandard2.0/OdysseyAnalytics.Core.AssemblyInfo.cs
+++ Core/obj/Debug/netstandard2.0/OdysseyAnalytics.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Core analytics SDK with clean architecture separation")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9abc4ec75e85947999fbd2b12093652795a5cc66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e7119780ed6a5d54f5c620a889a854556f9dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("odysseyAnalytics.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("odysseyAnalytics.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
